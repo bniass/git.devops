c696b4d17f1554d65ad5b30fb1ac8162cedb03bb
bbniass <bbniass@hotmail.com>
2020-02-28 13:32:24 +0000

    getter setter for prenom

--- Personne.java
+++ Personne.java
@@ -10,18 +10,16 @@ public class Personne{
         this.id = id;
     }
 
-<<<<<<< HEAD
     public String getPrenom(){
         return prenom;
     }
     public void setPrenom(String prenom){
         this.prenom = prenom;
-=======
+    }
     public String getNom(){
         return nom;
     }
     public void setNom(String nom){
         this.nom = nom;
->>>>>>> 871136568fa10c55bfd3b0e1fa169db543cf4929
     }
 }
