871136568fa10c55bfd3b0e1fa169db543cf4929
bniass <bniass@gmail.com>
2020-02-28 13:26:34 +0000

    commit getter.setter nom

--- Personne.java
+++ Personne.java
@@ -9,4 +9,11 @@ public class Personne{
     public void setId(int id){
         this.id = id;
     }
+
+    public String getNom(){
+        return nom;
+    }
+    public void setNom(String nom){
+        this.nom = nom;
+    }
 }
